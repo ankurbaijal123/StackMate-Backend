9e606d661b15c3710397bbad961f5e148ca12b49
ankur baijal <ankur.baijal11@gmail.com>
2025-03-28 15:10:46 +0530

    package.json added

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express();
+
+app.use("/test",(req, res) =>{
+    res.send("Hello from server using nodemon");
+})
+
+app.use("/hello",(req, res) =>{
+    res.send("Hello from Ankur");
+})
+
+app.listen(3000, () =>{
+    console.log("Server Started")
+})
