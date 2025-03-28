f08107c9f7eb66cdda4f32db66fe056bfcc8b4b7
ankur baijal <ankur.baijal11@gmail.com>
2025-03-28 18:59:34 +0530

    regular expression , regex and dynamic url of api

--- app.js
+++ app.js
@@ -2,27 +2,55 @@ const express = require("express")
 
 const app = express();
 
-app.use("/test/route",(req, res) =>{
+/* app.use("/user", (req,res)=>{
+    res.send("Hahahahahahahah")
+}) */
+    app.get(/.*fly$/, (req,res)=>{
+        res.send("Saved data to db")
+    }) // regex a
+    app.get("/ab?c", (req,res)=>{
+        res.send("Saved data to db")
+    }) // b is optional
+
+    app.get("/(ab)?c", (req,res)=>{
+        res.send("Saved data to db")
+    }) // ab is optional
+    
+
+
+    app.get("/ab+c", (req,res)=>{
+        res.send("Saved data to db")
+    })   // any number of b
+
+    app.get("/ab*c", (req,res)=>{
+        res.send("Saved data to db")
+    }) //any thing at place of *
+
+// This will match GET HTTP method API calls to /test
+app.get("/user/:userid", (req,res)=>{
+    console.log(req.params)
+    res.send({firstName:"Ankur", lastName: "Baijal"})
+})
+
+app.post("/user", (req,res)=>{
+    res.send("Saved data to db")
+})
+
+app.patch("/user", (req,res)=>{
+    res.send("Updated some data of user")
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("Deletd user")
+})
+// This will match all the HTTP method API calls to /test
+app.use("/test",(req, res) =>{
     res.send("Hello from server using nodemon test");
 })
 
 
 
-app.use("/hello",(req, res) =>{
-    res.send("Hello from Ankur");
-})
 
-app.use("/ankur",(req, res) =>{
-    res.send("Hello from Ankur Baijal");
-})
-
-app.use("test",(req, res) =>{
-    res.send("Hello from server using nodemon");
-})
-
-app.use("/",(req, res) =>{
-    res.send("Hello from server using nodemon");
-}) 
 app.listen(3000, () =>{
-    console.log("Server Started")
+    console.log("Server Startedat 3000")
 })
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 - Order of routes matter a lot
 - Postman app
 - make a workspace and test api call
+
+- write a logic to handle GET, POST, PATCH, DELETE API
