ccc89a6b3587be217538deb725fe1c27228127de
ankur baijal <ankur.baijal11@gmail.com>
2025-03-30 14:26:54 +0530

    trying multiple routes

--- app.js
+++ app.js
@@ -1,56 +1,34 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
 /* app.use("/user", (req,res)=>{
     res.send("Hahahahahahahah")
 }) */
-    app.get(/.*fly$/, (req,res)=>{
-        res.send("Saved data to db")
-    }) // regex a
-    app.get("/ab?c", (req,res)=>{
-        res.send("Saved data to db")
-    }) // b is optional
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("user api called");
+    next();
+    //res.send("user data here")
+  },
+  (req, res, next) => {
+    //res.send("user 2 data")
+    console.log("user 2 api called");
+    next();
+  },
+  (req, res, next) => {
+    //res.send("user 3 data")
+    console.log("user 3 api called");
+    next();
+  },
+  (req, res, next) => {
+    //res.send("user 4 data")
+    console.log("user 4 api called");
+    next()
+  }    
+); // code is executed line by line
 
-    app.get("/(ab)?c", (req,res)=>{
-        res.send("Saved data to db")
-    }) // ab is optional
-    
-
-
-    app.get("/ab+c", (req,res)=>{
-        res.send("Saved data to db")
-    })   // any number of b
-
-    app.get("/ab*c", (req,res)=>{
-        res.send("Saved data to db")
-    }) //any thing at place of *
-
-// This will match GET HTTP method API calls to /test
-app.get("/user/:userid", (req,res)=>{
-    console.log(req.params)
-    res.send({firstName:"Ankur", lastName: "Baijal"})
-})
-
-app.post("/user", (req,res)=>{
-    res.send("Saved data to db")
-})
-
-app.patch("/user", (req,res)=>{
-    res.send("Updated some data of user")
-})
-
-app.delete("/user", (req,res)=>{
-    res.send("Deletd user")
-})
-// This will match all the HTTP method API calls to /test
-app.use("/test",(req, res) =>{
-    res.send("Hello from server using nodemon test");
-})
-
-
-
-
-app.listen(3000, () =>{
-    console.log("Server Startedat 3000")
-})
+app.listen(3000, () => {
+  console.log("Server Started at 3000.....");
+});
